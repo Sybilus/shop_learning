6132c49350e0d15232366c6699f2688d99461fbe
Sybilus <88580591+Sybilus@users.noreply.github.com>
2022-09-02 01:56:04 +0200

    dark mode done

--- JS/carttoggle.js
+++ JS/carttoggle.js
@@ -4,18 +4,7 @@ let carttoggle = () => {
     let sidecart = document.getElementById('sidecart');
     sidecart.toggleAttribute('visible')
 }
-//header background when scroll
-function scrollFunction() {
-        if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
-            document.getElementById("header").style.backgroundColor = "#6074c1";
-            document.getElementById("header").style.borderTop = "solid 1px black";
-            document.getElementById("header").style.borderBottom = "solid 1px black";
-        } else {
-            document.getElementById("header").style.backgroundColor = "";
-            document.getElementById("header").style.borderTop = "";
-            document.getElementById("header").style.borderBottom = "";
-        }
-    }
+
 //go to top btn visibility after scroll
 let goTop = () => {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
@@ -24,22 +13,24 @@ let goTop = () => {
 }
 //back to top
 function topFunction() {
-  document.body.scrollTop = 0; 
-  document.documentElement.scrollTop = 0; 
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
 }
 
 // dark mode down
 
 const switchBtn = document.getElementById("switch");
 
-switchBtn.addEventListener("change", () => {
-    toggleClass();
-});
-let toggleClass = () => {
-let switcher = document.getElementsByClassName("header__in");
-switcher.toggleAttribute('Dark');
-}
+switchBtn.addEventListener('change', () => {
+    const switcher = document.getElementsByClassName('Whit');
+    
+    Array.from(switcher).forEach(function(target) {
+        if (!target.classList.contains('dark'))
+        return target.classList.add('dark');
+        else target.classList.remove('dark');
+    });
 
+});
 
 
 
